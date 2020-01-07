aedd0fb8883456e4d6aaf1cacf37e8ae2c4e7e61
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-07 17:05:43 -0500

    deconstructed values from userdata

--- src/Pages/UserDetailsPage/UserDetailsPage.js
+++ src/Pages/UserDetailsPage/UserDetailsPage.js
@@ -5,33 +5,33 @@ import { Avatar, StatBox } from "../../Components/";
 
 function UserDetailsPage() {
   const { fetchUserReducer: userData } = useSelector(state => state);
+  const {
+    login,
+    avatar_url,
+    html_url,
+    name,
+    bio,
+    public_repos,
+    followers,
+    following,
+    created_at
+  } = userData.userData;
   console.log(userData);
   return (
     <div className="UserDetailsPage-container">
       <header className="UserDetailsPage-user-header">
         <div className="UserDetailsPage-avatar-container">
-          <Avatar image={userData.userData.avatar_url} />
+          <Avatar image={avatar_url} />
         </div>
 
         <div className="UserDetailsPage-top-details">
           <div className="UserDetailsPage-user-name">
-            <h3 className="UserDetailsPage-login-h3">
-              {userData.userData.login}
-            </h3>
+            <h3 className="UserDetailsPage-login-h3">{login}</h3>
           </div>
           <div className="UserDetailsPage-stats-container">
-            <StatBox
-              number={userData.userData.public_repos}
-              stat={"Repos"}
-            ></StatBox>
-            <StatBox
-              number={userData.userData.followers}
-              stat={"Followers"}
-            ></StatBox>
-            <StatBox
-              number={userData.userData.following}
-              stat={"Following"}
-            ></StatBox>
+            <StatBox number={public_repos} stat={"Repos"}></StatBox>
+            <StatBox number={followers} stat={"Followers"}></StatBox>
+            <StatBox number={following} stat={"Following"}></StatBox>
           </div>
         </div>
       </header>
