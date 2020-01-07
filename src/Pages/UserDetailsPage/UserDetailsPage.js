7896625161e57b6a29755bede41dd912ed87ad75
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-07 14:06:16 -0500

    got data to userdetailspage

--- src/Pages/UserDetailsPage/UserDetailsPage.js
+++ src/Pages/UserDetailsPage/UserDetailsPage.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./UserDetailsPage.css";
 
 function UserDetailsPage() {
+  const { fetchUserReducer: userData } = useSelector(state => state);
+  console.log(userData);
   return (
     <div className="UserDetailsPage-container">
       <h2 className="UserDetailsPage-header">UserDetailsPage</h2>
