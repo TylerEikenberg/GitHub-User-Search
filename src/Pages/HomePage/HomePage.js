fe1f1248b7318fd44bef5e95dee87336fcfa5221
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-09 14:02:35 -0500

    Added content fo rhomepage

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -3,16 +3,11 @@ import "./HomePage.css";
 
 function HomePage() {
   return (
-    <div>
-      <div className="HomePage-container">
-        <h1 className="HomePage-title">GitHub User Search</h1>
-      </div>
-      <div className="HomePage-container">
-        <h2 className="HomePage-title">
-          Search for any GitHub user and get their stats, including number of
-          repos and followers.
-        </h2>
-      </div>
+    <div className="HomePage-container">
+      <h1 className="HomePage-title">GitHub User Search</h1>
+      <h2 className="HomePage-sub-heading">
+        a simple web application for finding GitHub users
+      </h2>
     </div>
   );
 }
