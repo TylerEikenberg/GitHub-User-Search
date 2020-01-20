b17d46e47d9c435314d8a2258eb8f641e13cf0fc
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-20 18:01:21 -0500

    Added more to fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './Redux/Reducers/';
-// changes made
+// changes made again
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
