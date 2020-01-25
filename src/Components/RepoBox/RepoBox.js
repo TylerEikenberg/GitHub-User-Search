357fb126d30170a58b7cc8659d81568bfb87476c
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-25 11:19:50 -0500

    changed a tag in repobox to h5

--- src/Components/RepoBox/RepoBox.js
+++ src/Components/RepoBox/RepoBox.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { StatBox } from "../";
-import "./RepoBox.css";
+import React from 'react';
+import { StatBox } from '../';
+import './RepoBox.css';
 
 function RepoBox({
   repoData = {
-    html_url: "",
-    name: "",
+    html_url: '',
+    name: '',
     stargazers_count: 0,
     forks: 0,
     watchers: 0
@@ -13,27 +13,19 @@ function RepoBox({
 }) {
   return (
     <a
-      data-testid="RepoBox-div"
-      className="RepoBox-container"
+      data-testid='RepoBox-div'
+      className='RepoBox-container'
       href={repoData.html_url}
-      target="_blank"
-      rel="noopener noreferrer"
+      target='_blank'
+      rel='noopener noreferrer'
     >
       <span>
-        <a
-          data-testid="RepoBox-a"
-          className="RepoBox-repo-link"
-          href={repoData.html_url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {repoData.name}
-        </a>
+        <h5 className='RepoBox-reponame'>{repoData.name}</h5>
       </span>
-      <span data-testid="RepoBox-span" className="RepoBox-stats-container">
-        <StatBox number={repoData.stargazers_count} stat={"Stars"} />
-        <StatBox number={repoData.forks} stat={"Forks"} />
-        <StatBox number={repoData.watchers} stat={"Watchers"} />
+      <span data-testid='RepoBox-span' className='RepoBox-stats-container'>
+        <StatBox number={repoData.stargazers_count} stat={'Stars'} />
+        <StatBox number={repoData.forks} stat={'Forks'} />
+        <StatBox number={repoData.watchers} stat={'Watchers'} />
       </span>
     </a>
   );
